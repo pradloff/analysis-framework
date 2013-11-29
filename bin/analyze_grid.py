f353e159b620f5917c53044a9b51fc95260a262c
pradloff <radloff@uoregon.edu>
2013-11-29 13:11:18 -0800

    fix grid analyzer

--- bin/analyze_grid.py
+++ bin/analyze_grid.py
@@ -63,7 +63,7 @@ def call_grid(
 	tarball.add('analyses')
 	tarball.close()
 
-	grid_command = 'echo %IN | sed \'s/,/\\n/g\' | sed \'s/ //g\' > input.txt; source analysis-framework/setup.sh; source analyses/setup.sh; analyze.py -m {module} -a {analysis} -t input.txt -o skim.root -p {processes} -n {tree}{keep}\{grl\}'.format(
+	grid_command = 'echo %IN | sed \'s/,/\\n/g\' | sed \'s/ //g\' > input.txt; source analysis-framework/setup.sh; source analyses/setup.sh; analyze.py -m {module} -a {analysis} -t input.txt -o skim.root -p {processes} -n {tree}{keep}{{grl}}'.format(
 		module=module_name,
 		analysis=analysis_name,
 		tree=tree,
