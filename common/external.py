06655c62c155f12d07cb323145fba6ab8f5fd068
pradloff <radloff@uoregon.edu>
2013-11-30 10:11:29 -0800

    fix loading externals

--- common/external.py
+++ common/external.py
@@ -64,6 +64,9 @@ def load(package,clean=False,verbose=False,level=0):
 		raise OSError('cmt directory not found in $ANALYSISHOME/external/{0}'.format(package))
 	os.chdir(cmtPath)
 
+	if package=='egammaAnalysisUtils' and not level:
+		print os.listdir('../StandAlone')
+
 	if clean: call('make -f Makefile.RootCore clean',verbose=verbose)
 
 	#get dependencies and load them first
