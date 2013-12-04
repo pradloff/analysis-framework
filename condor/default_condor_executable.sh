13ceefeb717e047de78d0dffc24f24345322e764
pradloff <radloff@uoregon.edu>
2013-12-04 08:42:44 -0800

    fix default condor submit

--- condor/default_condor_executable.sh
+++ condor/default_condor_executable.sh
@@ -7,6 +7,6 @@ echo "Setting up analysis tools"
 source {analysis_framework}/setup.sh
 source {analysis_home}/setup.sh
 echo "Running analysis slice"
-python -c "from condor.condor_analysis import analyze_slice_condor; analyze_slice_condor('{module_name}','{analysis_name}','{tree}',{grl},'{files}',{start},{end},'{output_name}',{process_number},'{error_file_name}','{logger_file_name},{keep}')"
+python -c "from condor.condor_analysis import analyze_slice_condor; analyze_slice_condor('{module_name}','{analysis_name}','{tree}',{grl},'{files}',{start},{end},'{output_name}',{process_number},'{error_file_name}','{logger_file_name}',{keep})"
 touch done
 
