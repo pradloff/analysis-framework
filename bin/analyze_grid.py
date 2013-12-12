1f8f5643c133b57e2d5085c6ba79ef4caa77d9ce
pradloff <radloff@uoregon.edu>
2013-12-12 09:54:54 -0800

    added to list of excluded grid sites

--- bin/analyze_grid.py
+++ bin/analyze_grid.py
@@ -98,7 +98,7 @@ def call_grid(
 				output_name=output_name,
 				merge=' --mergeOutput' if merge else '',
 				jobsize=jobsize,
-				exclude_sites='--excludedSite='+','.join(exclude_sites) if exclude_sites else '',
+				exclude_sites=' --excludedSite='+','.join(exclude_sites) if exclude_sites else '',
 				)
 
 			#print final_prun_command
