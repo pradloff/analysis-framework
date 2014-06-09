0cd3e3f926f498f9a6569a4058ddf32bcf3f0376
pradloff <radloff@uoregon.edu>
2014-06-09 14:15:03 -0700

    change tabs to spaces

--- bin/analyze.py
+++ bin/analyze.py
@@ -2,22 +2,22 @@
 
 if __name__ == '__main__':
 
-	import sys
-	import argparse
-	import os
+    import sys
+    import argparse
+    import os
     import itertools
 
-	parser = argparse.ArgumentParser(prog='analyze.py',description='Useful caller for analyses.')
-	parser.add_argument('-i','--input',default=[],dest='INPUT', nargs='+',help='Input file(s) to analyze.')
-	parser.add_argument('-t','--textinput',default=None,dest='TEXTINPUT',help='Text file containing input file(s) to analyze.  Separate files by line.')
-	parser.add_argument('-m','--module',dest='MODULE',required=True,help='Module containing analysis class.')
-	parser.add_argument('-a','--analysis',dest='ANALYSIS',required=True,help='Name of analysis to use.')
-	parser.add_argument('-o','--output',dest='OUTPUT',required=True,help='Name to give output ROOT file.')
-	parser.add_argument('--entries',default=None,dest='ENTRIES',help='Number of entries to process.')	
-	parser.add_argument('-n','--tree',dest='TREE',required=True,help='TTree name which contains event information.')
-	parser.add_argument('-g','--grl',default=[],dest='GRL',nargs='+',help='Good run list(s) XML file to use.')
-	parser.add_argument('-p','--processes',default=1,dest='PROCESSES',type=int,help='Number of processes to use.')
-	parser.add_argument('--keep',default=False,dest='KEEP',action='store_true',help='Keep all branches, default False')
+    parser = argparse.ArgumentParser(prog='analyze.py',description='Useful caller for analyses.')
+    parser.add_argument('-i','--input',default=[],dest='INPUT', nargs='+',help='Input file(s) to analyze.')
+    parser.add_argument('-t','--textinput',default=None,dest='TEXTINPUT',help='Text file containing input file(s) to analyze.  Separate files by line.')
+    parser.add_argument('-m','--module',dest='MODULE',required=True,help='Module containing analysis class.')
+    parser.add_argument('-a','--analysis',dest='ANALYSIS',required=True,help='Name of analysis to use.')
+    parser.add_argument('-o','--output',dest='OUTPUT',required=True,help='Name to give output ROOT file.')
+    parser.add_argument('--entries',default=None,dest='ENTRIES',help='Number of entries to process.')   
+    parser.add_argument('-n','--tree',dest='TREE',required=True,help='TTree name which contains event information.')
+    parser.add_argument('-g','--grl',default=[],dest='GRL',nargs='+',help='Good run list(s) XML file to use.')
+    parser.add_argument('-p','--processes',default=1,dest='PROCESSES',type=int,help='Number of processes to use.')
+    parser.add_argument('--keep',default=False,dest='KEEP',action='store_true',help='Keep all branches, default False')
 
     help = False
 
@@ -62,94 +62,94 @@ def analyze(
     entries,
     keep,
     help,
-	):
+    ):
 
-	class watcher():
-		def __init__(self,output,error,logger,child,prefix):
-			self.result = output	
-			self.error = error
-			self.logger = logger
-			self.child = child
-			self.prefix = prefix
+    class watcher():
+        def __init__(self,output,error,logger,child,prefix):
+            self.result = output    
+            self.error = error
+            self.logger = logger
+            self.child = child
+            self.prefix = prefix
 
-			self.error_file = None
-			self.logger_file = None
+            self.error_file = None
+            self.logger_file = None
 
-		def poll(self):
-			if all([
-				self.error_file is None,
-				os.path.exists(self.error)
-				]): self.error_file = open(self.error,'r+')
-			if all([
-				self.logger_file is None,
-				os.path.exists(self.logger)
-				]): self.logger_file = open(self.logger,'r+')
+        def poll(self):
+            if all([
+                self.error_file is None,
+                os.path.exists(self.error)
+                ]): self.error_file = open(self.error,'r+')
+            if all([
+                self.logger_file is None,
+                os.path.exists(self.logger)
+                ]): self.logger_file = open(self.logger,'r+')
 
-			exitcode = self.child.poll()
-			error = ''
-			logger = ''
+            exitcode = self.child.poll()
+            error = ''
+            logger = ''
 
-			if self.error_file: error = self.error_file.read()
-			if self.logger_file: logger = self.logger_file.read()
+            if self.error_file: error = self.error_file.read()
+            if self.logger_file: logger = self.logger_file.read()
 
-			if error: error = self.prefix+error.replace('\n','\n'+' '*len(self.prefix))
-			if logger: logger = self.prefix+logger.replace('\n','\n'+' '*len(self.prefix))
+            if error: error = self.prefix+error.replace('\n','\n'+' '*len(self.prefix))
+            if logger: logger = self.prefix+logger.replace('\n','\n'+' '*len(self.prefix))
 
-			return error,logger,exitcode	
+            return error,logger,exitcode    
 
-		def kill(self):
-			try: self.child.kill()
-			except OSError: pass
-			if self.error_file: self.error_file.close()
-			if self.logger_file: self.logger_file.close()
+        def kill(self):
+            try: self.child.kill()
+            except OSError: pass
+            if self.error_file: self.error_file.close()
+            if self.logger_file: self.logger_file.close()
 
-	full_output = os.path.abspath(output)
+    full_output = os.path.abspath(output)
 
-	analysis_constructor = __import__(module_name,globals(),locals(),[analysis_name]).__dict__[analysis_name]
+    analysis_constructor = __import__(module_name,globals(),locals(),[analysis_name]).__dict__[analysis_name]
 
     analysis_instance = analysis_constructor()
     if help: sys.exit(2)
 
-	generate_dictionaries()
+    generate_dictionaries()
 
-	print 'Validating analysis'
+    print 'Validating analysis'
 
-	analysis_instance.tree = tree
-	analysis_instance.grl = grl
-	analysis_instance.add_file(*files)
-	analysis_instance.setup_chain()
+    analysis_instance.tree = tree
+    analysis_instance.grl = grl
+    analysis_instance.add_file(*files)
+    analysis_instance.setup_chain()
 
-	print 'Analysis validated'
+    print 'Analysis validated'
 
-	while True:
-		directory = '/tmp/'+''.join(random.choice(string.ascii_uppercase + string.digits) for x in range(10))
-		try: os.mkdir(directory)
-		except OSError as error:
-			if error.errno != 17: raise
-			continue
-		break
-	print 'Created temporary directory {0}'.format(directory)
+    while True:
+        directory = '/tmp/'+''.join(random.choice(string.ascii_uppercase + string.digits) for x in range(10))
+        try: os.mkdir(directory)
+        except OSError as error:
+            if error.errno != 17: raise
+            continue
+        break
+    print 'Created temporary directory {0}'.format(directory)
 
-	cwd = os.getcwd()
-	atexit.register(os.chdir,cwd)
-	atexit.register(shutil.rmtree,os.path.abspath(directory))
-	os.chdir(directory)
+    cwd = os.getcwd()
+    atexit.register(os.chdir,cwd)
+    atexit.register(shutil.rmtree,os.path.abspath(directory))
+    os.chdir(directory)
 
-	if entries is not None:
-		entries = min([int(entries),analysis_instance.pchain.get_entries()])
-	else: entries = analysis_instance.pchain.get_entries()
+    if entries is not None:
+        entries = min([int(entries),analysis_instance.pchain.get_entries()])
+    else: entries = analysis_instance.pchain.get_entries()
 
-	del analysis_instance
+    del analysis_instance
 
-	ranges = [[i*(entries/num_processes),(i+1)*(entries/num_processes)] for i in range(num_processes)]
-	ranges[-1][-1]+= entries%(num_processes)
+    ranges = [[i*(entries/num_processes),(i+1)*(entries/num_processes)] for i in range(num_processes)]
+    ranges[-1][-1]+= entries%(num_processes)
 
-	files_text = 'files.txt'
-	with open(files_text,'w') as f:
-		for file_ in files: f.write(file_+'\n')
+    files_text = 'files.txt'
+    with open(files_text,'w') as f:
+        for file_ in files: f.write(file_+'\n')
 
-	#Start children
-	print 'Processing {0} entries with {1} processes'.format(entries,num_processes)
+    #Start children
+    print 'Processing {0} entries with {1} processes'.format(entries,num_processes)
 
     args = []
 
@@ -158,103 +158,103 @@ def analyze(
         g=list(g)
         args += g[:]
 
-	watchers = []
-	for process_number in range(num_processes):
-		if num_processes>1: suffix = '_{0:0>{1}}'.format(process_number,int(log(num_processes-1,10))+1)
-		else: suffix = ''
-		
-		start,end = ranges[process_number]
-		output = 'result{0}.root'.format(suffix)
-		error = 'error{0}.out'.format(suffix)
-		logger = 'logger{0}.out'.format(suffix)
+    watchers = []
+    for process_number in range(num_processes):
+        if num_processes>1: suffix = '_{0:0>{1}}'.format(process_number,int(log(num_processes-1,10))+1)
+        else: suffix = ''
+        
+        start,end = ranges[process_number]
+        output = 'result{0}.root'.format(suffix)
+        error = 'error{0}.out'.format(suffix)
+        logger = 'logger{0}.out'.format(suffix)
 
-		child_call = 'analyze_singlet.py -a {analysis_name} -m {module_name} -n {tree} -s {start} -e {end} -t {files_text} -o {output} -z {error} -l {logger}{keep}{grl} {args}'.format(
-			analysis_name = analysis_name,
-			module_name = module_name,
-			tree = tree,
-			start = start,
-			end = end,
-			files_text = files_text,
-			output = output,
-			error = error,
-			logger = logger,
-			keep = ' --keep' if keep else '',
-			grl = ' -g {0}'.format(' '.join(grl)) if grl else '',
+        child_call = 'analyze_singlet.py -a {analysis_name} -m {module_name} -n {tree} -s {start} -e {end} -t {files_text} -o {output} -z {error} -l {logger}{keep}{grl} {args}'.format(
+            analysis_name = analysis_name,
+            module_name = module_name,
+            tree = tree,
+            start = start,
+            end = end,
+            files_text = files_text,
+            output = output,
+            error = error,
+            logger = logger,
+            keep = ' --keep' if keep else '',
+            grl = ' -g {0}'.format(' '.join(grl)) if grl else '',
             args = ' '.join(args),
-			)
-		watchers.append(watcher(output,error,logger,subprocess.Popen(child_call.split()),'Process {0}: '.format(process_number)))
+            )
+        watchers.append(watcher(output,error,logger,subprocess.Popen(child_call.split()),'Process {0}: '.format(process_number)))
 
-	#Monitor
-	results = []
-	exitcodes = []
-	finished = [False for i in range(num_processes)]
-	while True:
-		try:
-			sleep(1)
-			for process_number,watcher in enumerate(watchers):
-				logger,error,exitcode = watcher.poll()
-				if logger: print logger.strip()
-				if error: print error.strip()
-				if exitcode is not None and not finished[process_number]:
-					if exitcode: print 'Process {0} failed'.format(process_number)
-					else: print 'Process {0} finished successfully'.format(process_number)
-					exitcodes.append(exitcode)
-					results.append(watcher.result)
-					finished[process_number]=True
-			if all(finished): break
+    #Monitor
+    results = []
+    exitcodes = []
+    finished = [False for i in range(num_processes)]
+    while True:
+        try:
+            sleep(1)
+            for process_number,watcher in enumerate(watchers):
+                logger,error,exitcode = watcher.poll()
+                if logger: print logger.strip()
+                if error: print error.strip()
+                if exitcode is not None and not finished[process_number]:
+                    if exitcode: print 'Process {0} failed'.format(process_number)
+                    else: print 'Process {0} finished successfully'.format(process_number)
+                    exitcodes.append(exitcode)
+                    results.append(watcher.result)
+                    finished[process_number]=True
+            if all(finished): break
 
-		except KeyboardInterrupt:
-			for watcher in watchers:
-				watcher.kill()
-			break
+        except KeyboardInterrupt:
+            for watcher in watchers:
+                watcher.kill()
+            break
 
-	if any(exitcodes) or not all(finished):
-		print 'Abnormal exit in at least one process, terminating'
-		sys.exit(1)
+    if any(exitcodes) or not all(finished):
+        print 'Abnormal exit in at least one process, terminating'
+        sys.exit(1)
 
-	for watcher in watchers: watcher.kill()
+    for watcher in watchers: watcher.kill()
 
-	os.chdir(cwd)
-	mkpath(os.path.dirname(full_output))
-	if num_processes>1:
-		merger = ROOT.TFileMerger()
-		if os.path.exists(full_output): os.remove(full_output)
-		merger.OutputFile(full_output)
-		for result in results:
-			merger.AddFile(directory+'/'+result)
-		merger.Merge()
-	else:
-		shutil.move(directory+'/'+results[0],full_output)
+    os.chdir(cwd)
+    mkpath(os.path.dirname(full_output))
+    if num_processes>1:
+        merger = ROOT.TFileMerger()
+        if os.path.exists(full_output): os.remove(full_output)
+        merger.OutputFile(full_output)
+        for result in results:
+            merger.AddFile(directory+'/'+result)
+        merger.Merge()
+    else:
+        shutil.move(directory+'/'+results[0],full_output)
 
 if __name__ == '__main__':
 
-	files = []
-	
-	if args.INPUT:
-		files += [file_ if ':' in file_ else os.path.abspath(file_) for file_ in args.INPUT]
-	
-	if args.TEXTINPUT:
-		with open(args.TEXTINPUT) as f:
-			for line in f.readlines():
-				if not line.strip(): continue
-				file_ = line.strip()
-				files.append(file_ if ':' in file_ else os.path.abspath(file_))
+    files = []
+    
+    if args.INPUT:
+        files += [file_ if ':' in file_ else os.path.abspath(file_) for file_ in args.INPUT]
+    
+    if args.TEXTINPUT:
+        with open(args.TEXTINPUT) as f:
+            for line in f.readlines():
+                if not line.strip(): continue
+                file_ = line.strip()
+                files.append(file_ if ':' in file_ else os.path.abspath(file_))
 
-	if not files:
-		print 'No input found, exiting'
-		sys.exit(1)
+    if not files:
+        print 'No input found, exiting'
+        sys.exit(1)
         #exit = True
 
-	analyze(
-		args.MODULE,
-		args.ANALYSIS,
-		files,
-		args.TREE,
-		args.GRL,
-		args.PROCESSES,
-		args.OUTPUT,
-		args.ENTRIES,
-		args.KEEP,
+    analyze(
+        args.MODULE,
+        args.ANALYSIS,
+        files,
+        args.TREE,
+        args.GRL,
+        args.PROCESSES,
+        args.OUTPUT,
+        args.ENTRIES,
+        args.KEEP,
         help,
-		)
+        )
 
--- bin/analyze_singlet.py
+++ bin/analyze_singlet.py
@@ -2,22 +2,22 @@
 
 if __name__=='__main__':
 
-	import sys
-	import argparse
-	import atexit
+    import sys
+    import argparse
+    import atexit
 
-	parser = argparse.ArgumentParser(prog='analyze_singlet.py',description='Useful caller for analyses with single process.')
-	parser.add_argument('-t','--textinput',dest='TEXTINPUT',required=True,help='Text file containing input file(s) to analyze.  Separate files by line.')
-	parser.add_argument('-m','--module',dest='MODULE',required=True,help='Module containing analysis class.')
-	parser.add_argument('-a','--analysis',dest='ANALYSIS',required=True,help='Name of analysis to use.')
-	parser.add_argument('-o','--output',dest='OUTPUT',required=True,help='Name to give output ROOT file.')
-	parser.add_argument('-l','--logger',dest='LOGGER',required=True,help='Name to give output logger file.')
-	parser.add_argument('-z','--error',dest='ERROR',required=True,help='Name to give error logger file.')
-	parser.add_argument('-s',type=int,dest='START',required=True,help='Entry to start processing.')
-	parser.add_argument('-e',type=int,dest='END',required=True,help='Entry to end processing.')
-	parser.add_argument('-n','--tree',dest='TREE',required=True,help='TTree name which contains event information.')
-	parser.add_argument('-g','--grl',default=[],dest='GRL',nargs='+',help='Good run list(s) XML file to use.')
-	parser.add_argument('--keep',default=False,dest='KEEP',action='store_true',help='Keep all branches, default False')
+    parser = argparse.ArgumentParser(prog='analyze_singlet.py',description='Useful caller for analyses with single process.')
+    parser.add_argument('-t','--textinput',dest='TEXTINPUT',required=True,help='Text file containing input file(s) to analyze.  Separate files by line.')
+    parser.add_argument('-m','--module',dest='MODULE',required=True,help='Module containing analysis class.')
+    parser.add_argument('-a','--analysis',dest='ANALYSIS',required=True,help='Name of analysis to use.')
+    parser.add_argument('-o','--output',dest='OUTPUT',required=True,help='Name to give output ROOT file.')
+    parser.add_argument('-l','--logger',dest='LOGGER',required=True,help='Name to give output logger file.')
+    parser.add_argument('-z','--error',dest='ERROR',required=True,help='Name to give error logger file.')
+    parser.add_argument('-s',type=int,dest='START',required=True,help='Entry to start processing.')
+    parser.add_argument('-e',type=int,dest='END',required=True,help='Entry to end processing.')
+    parser.add_argument('-n','--tree',dest='TREE',required=True,help='TTree name which contains event information.')
+    parser.add_argument('-g','--grl',default=[],dest='GRL',nargs='+',help='Good run list(s) XML file to use.')
+    parser.add_argument('--keep',default=False,dest='KEEP',action='store_true',help='Keep all branches, default False')
 
     args = []
     for i,(k,g) in enumerate(itertools.groupby(sys.argv,lambda x:x=='-')):
@@ -25,23 +25,23 @@ if __name__=='__main__':
         args += g[1:]
         break
 
-	args = parser.parse_args(args)
+    args = parser.parse_args(args)
 
-	from common.analysis import analyze_slice
+    from common.analysis import analyze_slice
 
-	singlet = analyze_slice(
-		args.MODULE,
-		args.ANALYSIS,
-		args.TREE,
-		args.GRL,
-		args.TEXTINPUT,
-		args.START,
-		args.END,
-		args.OUTPUT,
-		args.ERROR,
-		args.LOGGER,
-		args.KEEP,
-		)
-	atexit.register(singlet.cleanup)
-	singlet.initialize()
-	singlet.run()
+    singlet = analyze_slice(
+        args.MODULE,
+        args.ANALYSIS,
+        args.TREE,
+        args.GRL,
+        args.TEXTINPUT,
+        args.START,
+        args.END,
+        args.OUTPUT,
+        args.ERROR,
+        args.LOGGER,
+        args.KEEP,
+        )
+    atexit.register(singlet.cleanup)
+    singlet.initialize()
+    singlet.run()
