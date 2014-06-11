629a29f21a507571da904bf81ede80f037161f2a
pradloff <radloff@uoregon.edu>
2014-06-11 13:26:53 -0700

    fixes

--- bin/analyze_singlet.py
+++ bin/analyze_singlet.py
@@ -5,6 +5,7 @@ if __name__=='__main__':
     import sys
     import argparse
     import atexit
+    import itertools
 
     parser = argparse.ArgumentParser(prog='analyze_singlet.py',description='Useful caller for analyses with single process.')
     parser.add_argument('-t','--textinput',dest='TEXTINPUT',required=True,help='Text file containing input file(s) to analyze.  Separate files by line.')
