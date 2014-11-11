bc487d5a3e99ecb8b0523c1ab6d43675184b75f4
pradloff <radloff@uoregon.edu>
2014-11-11 08:52:43 -0600

    update pchain to new branch style

--- common/branches.py
+++ common/branches.py
@@ -3,35 +3,32 @@ ROOT.gROOT.ProcessLine("gErrorIgnoreLevel = 2001;")
 from functions import memoize
 import os
 import tempfile
+from helper import root_quiet
 
 @memoize
 def generate_dictionary(name,base):
     cwd = os.getcwd()
     os.chdir(tempfile.mkdtemp())
-    ROOT.gInterpreter.GenerateDictionary(name,base)
+    with root_quiet(filters=["TClassTable::Add:0: RuntimeWarning","Note: Link requested for already"]):
+	    ROOT.gInterpreter.GenerateDictionary(name,base)
     os.chdir(cwd)
     
 class branch(object):
     def __init__(self,name,type_):
     	self.name = name
     	self.type = type_
-    #@property
-    #def value(self): raise NotImplementedError
-    
-    #@value.setter
-    #def value(self,value):
-    #	pass
-    #	#self.value = 
+    	self.tbranch = None
     	
    	def generate_dictionary(self): pass
 
 	def link(self,pchain): raise NotImplementedError
 
+	def get_entry(self,entry): self.tbranch.GetEntry(entry)
+
     def read(self,pchain):
 		self.generate_dictionary()
 		pchain().SetBranchStatus(self.name,1)
 		self.link(pchain)
-		
 
 class vector_branch(branch):
 	def __init__(self,name,type_):
@@ -44,7 +41,7 @@ class vector_branch(branch):
 		self.value = getattr(ROOT,self.type)()
 		pchain().SetBranchAddress(self.name,ROOT.AddressOf(self.value))
 
-
+	def get_value(self): return self.value
 
 class std_branch(branch):
 
@@ -65,6 +62,8 @@ class std_branch(branch):
 		if self.type not in lookup: raise TypeError('Unknown branch type {0}'.format(self.type))
 		self.value = array(lookup[self.type],[0])
 		pchain().SetBranchAddress(self.name,ROOT.AddressOf(self.value))	
+
+	def get_value(self): return self.value[0]
 
 #class
 
--- common/pchain.py
+++ common/pchain.py
@@ -1,60 +1,6 @@
 import ROOT
 ROOT.gROOT.ProcessLine("gErrorIgnoreLevel = 2001;")
-import re
-from array import array
-import os
-
-from functions import memoize
-
-class branch()
-    def __init__(self,name,base)
-
-
-
-
-def generate_dictionaries():
-	cwd = os.getcwd()
-	try: os.mkdir('dictionaries')
-	except OSError: pass
-	os.chdir('dictionaries')
-	for name,base in [
-		("vector<vector<float> >","vector"),
-		("vector<vector<int> >","vector"),
-		("vector<vector<unsigned int> >","vector"),
-		("vector<vector<string> >","vector"),
-		("vector<vector<vector<float> > >","vector"),
-		("vector<vector<vector<int> > >","vector"),
-		("vector<vector<vector<unsigned int> > >","vector"),
-		('vector<TLorentzVector>','vector;TLorentzVector.h'),
-		]: ROOT.gInterpreter.GenerateDictionary(name,base)
-	os.chdir(cwd)
-	return cwd+'/dictionaries'
-
-"""
-def generate_dictionaries():
-	cwd = os.getcwd()
-	try: os.mkdir('dictionaries')
-	except OSError as error:
-		if error.errno!=17: raise 
-	os.chdir('dictionaries')
-	if not os.path.exists('Loader.C'):
-		with open('Loader.C','w') as f:
-			for line in [
-				'#include "TLorentzVector.h"',
-				'#include <vector>',
-				'#ifdef __MAKECINT__',
-				'#pragma link C++ class vector<vector<bool> >+;',
-				'#pragma link C++ class vector<vector<int> >+;',
-				'#pragma link C++ class vector<vector<double> >+;',
-				'#pragma link C++ class vector<vector<string> >+;',
-				'#pragma link C++ class vector<vector<unsigned int> >+;',
-				'#pragma link C++ class vector<vector<float> >+;',
-				'#pragma link C++ class vector<TLorentzVector>+;',
-				'#endif',
-				]: f.write(line+'\n')
-        ROOT.gROOT.ProcessLine('.L {0}+'.format(os.path.abspath('Loader.C')))
-	os.chdir(cwd)
-"""
+from common.branches import vector_branch, std_branch
 
 class pchain():
 
@@ -66,7 +12,7 @@ class pchain():
 		self.chain.SetCacheLearnEntries(10)
 
 		#self.branch_names = []
-		self.branch_types = {}
+		#self.branch_types = {}
 		#self.branch_values = {}
 		self.branches = {}
 		#self.created_branches = {}
@@ -90,14 +36,7 @@ class pchain():
 		for f in files:
 			self.validate_file(f)
 			self.files.append(f)
-			
-			self.files_branches[f] = [branch.GetName() for branch in tree.GetListOfBranches()]
-			for leaf in tree.GetListOfLeaves():
-				if leaf.GetName() not in self.branch_types: self.branch_types[leaf.GetName()] = leaf.GetTypeName()
-				elif leaf.GetTypeName()!=self.branch_types[leaf.GetName()]:
-					raise TypeError('Branch typing for {0} changes in file {1}'.format(leaf.GetName(),f))
-			tfile.Close()
-			self.chain.Add(f)
+			self().Add(f)
 			
     def validate_file(self,f):
 		tfile = ROOT.TFile.Open(f)
@@ -108,111 +47,45 @@ class pchain():
 			not isinstance(tree,ROOT.TTree),
 			]): raise ValueError,'No matches for TTree "{0}" in file {1}.'.format(self.tree,f)
 		if self.files:
+			branch_types = {}
 			for leaf in tree.GetListOfLeaves():
-				if leaf.GetName() not in self.branch_types: self.branch_types[leaf.GetName()] = leaf.GetTypeName()
-				elif leaf.GetTypeName()!=self.branch_types[leaf.GetName()]:
-					raise TypeError('Branch typing for {0} changes in file {1}'.format(leaf.GetName(),f))			
-			
-			
-	def get_available_branch_names(self,required=True):
-		#require all trees in all files to have the requested branch
-		if required:
-			if not self.branches_intersection: 
-				file_branches = self.files_branches.values()
-				if not file_branches: self.branches_intersection = []
-				elif len(file_branches)==1: self.branches_intersection = file_branches[0]
-				else: self.branches_intersection = list(set(file_branches[0]).intersection(*[set(branches) for branches in file_branches[1:]]))
-			return self.branches_intersection
-		#All other types allow the possibility to react to different branches in trees
+				type_ = leaf.GetTypeName()
+				name = leaf.GetName()
+				d[name] = type_
+			for branch_name,branch in self.branches.items():
+				if any([
+					branch_name not in branch_types,
+					branch_types[branch_name] != branch.type,
+					]): raise RuntimeError('Mis-match data between original file {0} and appended file {1} in branch {2}'.format(self.files[0],f,branch_name))		
 		else:
-			if self.branches_union is None: 
-				file_branches = self.files_branches.values()
-				if not file_branches: self.branches_union = []
-				elif len(file_branches)==1: self.branches_union = file_branches[0]
-				else: self.branches_union = list(set(file_branches[0]).union(*[set(branches) for branches in file_branches[1:]]))
-			return self.branches_union
-
-	#creates virtual branch
-	def create_branch(self,branch_name,event_function_name):
-		if branch_name in self.created_branches: return
-		self.created_branches[branch_name] = event_function_name
-		if branch_name in self.get_available_branch_names(required=False):
-			self.set_branch(branch_name)
-			self.branch_names.append(branch_name)
-
-	def create_branches(self,branch_names,event_function_name):
-		for branch_name in branch_names: self.create_branch(branch_name,event_function_name)		
+			for leaf in tree.GetListOfLeaves():
+				type_ = leaf.GetTypeName()
+				name = leaf.GetName()
+				if type_.startswith('vector'): self.branches[name] = vector_branch(name,type_)
+				else: self.branches[name] = std_branch(name,type_)
+		tfile.Close()
 
 	def request_branch(self,branch_name):
-		if branch_name in self.created_branches: return
-		if branch_name in self.branch_names: return
-		if branch_name not in self.get_available_branch_names(required=True):
-			raise ValueError('No matches for required branch {0}'.format(branch_name))
-		self.set_branch(branch_name)
-		self.branch_names.append(branch_name)
+		self.branches[branch_name].read(self)
 
 	def request_branches(self,branch_names):
 		for branch_name in branch_names: self.request_branch(branch_name)
 
 	def set_entry(self,entry):
-		self.current_entry = self.chain.LoadTree(entry)
-		file_number = self.chain.GetTreeNumber()
-		#reset if file changed
+		self.current_entry = self().LoadTree(entry)
+		file_number = self().GetTreeNumber()
+
 		if self.current_file_number != file_number:
 			current_file = self.files[self.current_file_number]
 			self.first_entry_files.append(current_file)
 			self.current_file_number = file_number
-			self.branches = {}
-			for branch_name in set(self.branch_names)&set(self.files_branches[current_file]):
-				self.branches[branch_name] = self.chain.GetBranch(branch_name)
+			for branch_name,branch in self.branches.items():
+				branch.tbranch = self().GetBranch(branch_name)
 		
-	def get_branches(self,event,branch_names,event_function_name):
-		#Add values to event which are registered and are not already in event:
-		for branch_name in set(branch_names)&set(self.branch_names)-set(event.__dict__.keys()):
-
-			try: self.branches[branch_name].GetEntry(self.current_entry)
-			except KeyError:
-				try: assert(self.created_branches[branch_name]==event_function_name)
-				except KeyError,AssertionError: raise ValueError('Branch {0} not created as promised by event function {1}'.format(branch_name,self.created_branches[branch_name]))
-
+	def get_branches(self,event,branch_names):
+		for branch_name in branch_names:
+			if branch_name not in self.branches: raise KeyError('Unknown branch {0}'.format(branch_name))
+			branch = self.branches[branch_name]
+			branch.get_entry(self.current_entry)
+			event.__dict__[branch_name] = branch.get_value()
 			value = self.branch_values[branch_name]
-			#pull single value from array
-			if isinstance(value,array): event.__dict__[branch_name] = value[0]
-			else: event.__dict__[branch_name] = value
-		for branch_name in set(branch_names)-set(self.branch_names)-set(event.__dict__.keys()):
-			if not self.created_branches[branch_name]==event_function_name:
-				raise ValueError('Branch {0} not created as promised by event function {1}'.format(branch_name,self.created_branches[branch_name]))			
-			
-	def set_branch(self,branch_name):
-		branch_type = self.branch_types[branch_name]
-		
-
-		if branch_type in ['Char_t','Int_t','Bool_t','UInt_t']:
-			self.branch_values[branch_name]=array('i',1*[0])
-			self.chain.SetBranchStatus(branch_name,1)
-			self.chain.SetBranchAddress(branch_name,self.branch_values[branch_name])
-		elif branch_type == 'Long64_t':
-			self.branch_values[branch_name]=array('l',1*[0])
-			self.chain.SetBranchStatus(branch_name,1)
-			self.chain.SetBranchAddress(branch_name,self.branch_values[branch_name])
-		elif branch_type == 'Float_t':
-			self.branch_values[branch_name]=array('f',1*[0.0])
-			self.chain.SetBranchStatus(branch_name,1)
-			self.chain.SetBranchAddress(branch_name,self.branch_values[branch_name])
-		elif branch_type == 'Double_t':
-			self.branch_values[branch_name]=array('d',1*[0.0])
-			self.chain.SetBranchStatus(branch_name,1)
-			self.chain.SetBranchAddress(branch_name,self.branch_values[branch_name])			
-		elif branch_type == 'string':
-			self.branch_values[branch_name]=ROOT.std.string()
-			self.chain.SetBranchStatus(branch_name,1)
-			self.chain.SetBranchAddress(branch_name,ROOT.AddressOf(self.branch_values[branch_name]))
-		elif branch_type.startswith('vector'):
-			try:
-				self.branch_values[branch_name] = getattr(ROOT,branch_type)()
-			except AttributeError:
-				raise TypeError('Unknown ROOT type {0} for branch {1}'.format(branch_type,branch_name))
-			self.chain.SetBranchStatus(branch_name,1)
-			self.chain.SetBranchAddress(branch_name,ROOT.AddressOf(self.branch_values[branch_name]))
-		else:
-			raise ValueError('Branch {0} could not be configured, type "{1}" not supported'.format(branch_name,branch_type))
